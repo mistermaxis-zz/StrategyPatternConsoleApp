f8680a5705d7fd1b1ca2ea21a906b76be2a8b66f
mistermaxis <55718118+mistermaxis@users.noreply.github.com>
2019-12-29 15:44:37 -0300

    More or less this is what the strategy pattern is about. Using different algorithms but just one object. I'd like to improve upon the algorithms after some research.

--- StrategyPatternConsoleApp/Encrypters.h
+++ StrategyPatternConsoleApp/Encrypters.h
@@ -1,25 +1,111 @@
 #pragma once
 #include <string>
+#include <iostream>
+#include <vector>
 
 namespace Encrypters {
 
 	class EncrypterAlgo {
 
 	public:
-		virtual void Encrypt(std::string source) = 0;
-		virtual void Decrypt(std::string source) = 0;
+		virtual void Encrypt(const std::string source, std::vector<unsigned long>& code) = 0;
+		virtual void Decrypt(std::string& source, const std::vector<unsigned long> code) = 0;
 
 	};
 
-	class Encrypter {
+	class SuperEncrypt : public EncrypterAlgo
+	{
+		virtual void Encrypt(const std::string source, std::vector<unsigned long>& code)
+		{
+			unsigned long long charCode = 0;
 
+			for (auto i = source.cbegin(); i != source.cend(); i++)
+			{
+				charCode = (unsigned long long)*i;
+				charCode = charCode + 41 * 23 - 3 * 1111;
+				code.push_back(charCode);
+			}
+		}
+
+		virtual void Decrypt(std::string& source, const std::vector<unsigned long> code)
+		{
+			unsigned long long stringCode;
+
+			for (auto i = code.cbegin(); i != code.cend(); i++)
+			{
+				stringCode = *i;
+				stringCode = stringCode - 41 / 23 + 3 / 1111;
+				source.push_back(stringCode);
+			}
+		}
+	};
+
+	class HyperEncrypt : protected EncrypterAlgo
+	{
+		virtual void Encrypt(const std::string source, std::vector<unsigned long>& code)
+		{
+			unsigned long long charCode = 0;
+
+			for (auto i = source.cbegin(); i != source.cend(); i++)
+			{
+				charCode = (unsigned long long) * i;
+				charCode = charCode * 31416;
+				code.push_back(charCode);
+			}
+		}
+
+		virtual void Decrypt(std::string& source, const std::vector<unsigned long> code)
+		{
+			unsigned long long stringCode;
+
+			for (auto i = code.cbegin(); i != code.cend(); i++)
+			{
+				stringCode = *i;
+				stringCode = stringCode / 31416;
+				source.push_back(stringCode);
+			}
+		}
+	};
+
+	class MegaEncrypt : public EncrypterAlgo
+	{
+		virtual void Encrypt(const std::string source, std::vector<unsigned long>& code)
+		{
+			unsigned long long charCode = 0;
+
+			for (auto i = source.cbegin(); i != source.cend(); i++)
+			{
+				charCode = (unsigned long long) * i;
+				charCode = charCode + 7;
+				code.push_back(charCode);
+			}
+		}
+
+		virtual void Decrypt(std::string& source, const std::vector<unsigned long> code)
+		{
+			unsigned long long stringCode;
+
+			for (auto i = code.cbegin(); i != code.cend(); i++)
+			{
+				stringCode = *i;
+				stringCode = stringCode - 7;
+				source.push_back(stringCode);
+			}
+		}
+	};
+
+	class Encrypter {
 	private:
 		EncrypterAlgo* Algorithm;
-
+		std::vector<std::string> EncryptedStrings;
+		std::vector<std::string> DecryptedStrings;
+		std::vector<unsigned long> Code;
 	public:
-		void SelectAlgorithm(EncrypterAlgo&& algorithm) { Algorithm = &algorithm; }
-		void EncryptString(std::string&& str) { Algorithm->Encrypt(str); }
-		void DecryptString(std::string&& str) { Algorithm->Decrypt(str); }
+		void SelectAlgorithm(EncrypterAlgo& algorithm) { Algorithm = &algorithm; }
+		void EncryptString(std::string&& str) { EncryptedStrings.push_back(str); Algorithm->Encrypt(str, Code); }
+		void DecryptString(std::vector<unsigned long> code) {Algorithm->Decrypt(EncryptedStrings[0], code); }
+		void PresentEncryptedString() { std::cout << EncryptedStrings[0] << "\n"; }
+		void PresentCode() { for (auto i : Code) { std::cout << i << " "; } std::cout << "\n"; }
 	};
 
 }
--- StrategyPatternConsoleApp/StrategyPatternConsoleApp.cpp
+++ StrategyPatternConsoleApp/StrategyPatternConsoleApp.cpp
@@ -3,5 +3,24 @@
 
 int main()
 {
-    
+    std::string test = "This is a test string";
+
+    Encrypters::Encrypter encrypter;
+
+    Encrypters::MegaEncrypt megaEncrypt;
+    Encrypters::SuperEncrypt superEncrypt;
+
+    encrypter.SelectAlgorithm(megaEncrypt);
+
+    encrypter.EncryptString("this is a string");
+
+    encrypter.PresentCode();
+
+    encrypter.SelectAlgorithm(superEncrypt);
+
+    encrypter.EncryptString("This is a string");
+
+    encrypter.PresentCode();
+
+    return 0;
 }
